af878e04cfc468e76c9341d3910bef5fd3b178a2
Slaterc6490 <146682294+Slaterc6490@users.noreply.github.com>
2023-11-09 21:05:59 -0500

    Add files via upload

---
+++ p4LAB1a_SlaterChristian.py
@@ -0,0 +1,10 @@
+import turtle
+turtle.showturtle()
+turtle.shape("turtle")
+for x in range(4):
+    turtle.forward(50) # move turtle forward
+    turtle.right(90) # turn turtle right (90 degree's)
+
+for a in range(3):
+  turtle.forward(50) # Move turtle forward
+  turtle.left(120) 
